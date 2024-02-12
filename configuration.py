a0d88b129d1ab2945730624a79400dfd85f004e4
unknown <ben.romero24@gmail.com>
2024-02-11 19:00:35 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,13 @@
+URL_SERVICE = "https://cnt-ed5c34f7-6ef4-4dd1-98b7-ea05bae0d6c2.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/" #obtener logs
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv" #obtener datos de la tabla de la DB de usuarios
+
+CREATE_USER_PATH = "/api/v1/users/" #crear un usuario nuevo
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/" #búsqueda de kits por productos
+
+
+
